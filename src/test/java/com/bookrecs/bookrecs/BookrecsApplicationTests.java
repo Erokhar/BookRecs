d6ccc778d60c523c6330185e719fcc7e98295881
Adam Benrqia <adam.benrqia00@gmail.com>
2021-09-22 23:01:37 +0100

    Initial Commit
    Initial Commit

---
+++ src/test/java/com/bookrecs/bookrecs/BookrecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookrecs.bookrecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrecsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
